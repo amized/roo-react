aead5f0dff158a5002d0e559f7be30359b70cca3
Amiel Zwier <amized@gmail.com>
2017-09-10 15:10:28 -0400

    started implementing timeouts

--- src/main/decorators.js
+++ src/main/decorators.js
@@ -3,6 +3,21 @@ import _ from "lodash"
 
 const om = objectManager;
 
+let lastUpdated = Date.now();
+let throttleMs = 500;
+let updateBuffer = [];
+let updateTimeout = null;
+
+function throttledUpdate() {
+  const uniqTokens = _.uniq([].concat.apply([], updateBuffer));
+  om.notifyUpdate(uniqTokens);
+  updateBuffer = [];
+  lastUpdated = Date.now();
+  updateTimeout = null;
+}
+
+
+
 // Decorator function for method notifying a state change
 export function stateChange(target, name, descriptor) {
   let fn = descriptor.value;
@@ -11,16 +26,21 @@ export function stateChange(target, name, descriptor) {
   target.__roo_enabled = true;
   let newFn = function (...args) {
   	if (this.__roo) {
-  		om.updateBuffer.push(this.__roo.tokens);
+  		updateBuffer.push(this.__roo.tokens);
   	}
   	if (!om.isUpdating) {
 	  	om.isUpdating = true;
 	    const result = fn.apply(this, arguments);
-	    const uniqTokens = _.uniq([].concat.apply([], om.updateBuffer));
-	    om.notifyUpdate(uniqTokens);
-	    om.updateBuffer = [];
-	    om.isUpdating = false;
-	    return result;
+      if (updateTimeout === null) {
+        const now = Date.now();
+        const elapsed = now - lastUpdated;
+        const timeoutMs = elapsed < throttleMs ? throttleMs - elapsed : 0;
+        updateTimeout = setTimeout(() => {
+          throttledUpdate();
+        }, timeoutMs);
+      }
+      om.isUpdating = false;
+      return result;
   	}
   	return fn.apply(this, arguments);
   };
