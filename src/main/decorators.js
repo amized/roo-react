fca7258e33a8476dc0b9ae7e24b495da461c843e
Amiel Zwier <amized@gmail.com>
2017-10-22 15:25:39 -0400

    Removed timeouts work until it has more thought put into it

--- src/main/decorators.js
+++ src/main/decorators.js
@@ -3,21 +3,6 @@ import _ from "lodash"
 
 const om = objectManager;
 
-let lastUpdated = Date.now();
-let throttleMs = 500;
-let updateBuffer = [];
-let updateTimeout = null;
-
-function throttledUpdate() {
-  const uniqTokens = _.uniq([].concat.apply([], updateBuffer));
-  om.notifyUpdate(uniqTokens);
-  updateBuffer = [];
-  lastUpdated = Date.now();
-  updateTimeout = null;
-}
-
-
-
 // Decorator function for method notifying a state change
 export function stateChange(target, name, descriptor) {
   let fn = descriptor.value;
@@ -25,24 +10,19 @@ export function stateChange(target, name, descriptor) {
   // check that this object is a Roo object
   target.__roo_enabled = true;
   let newFn = function (...args) {
-  	if (this.__roo) {
-  		updateBuffer.push(this.__roo.tokens);
-  	}
-  	if (!om.isUpdating) {
-	  	om.isUpdating = true;
-	    const result = fn.apply(this, arguments);
-      if (updateTimeout === null) {
-        const now = Date.now();
-        const elapsed = now - lastUpdated;
-        const timeoutMs = elapsed < throttleMs ? throttleMs - elapsed : 0;
-        updateTimeout = setTimeout(() => {
-          throttledUpdate();
-        }, timeoutMs);
-      }
+    if (this.__roo) {
+      om.updateBuffer.push(this.__roo.tokens);
+    }
+    if (!om.isUpdating) {
+      om.isUpdating = true;
+      const result = fn.apply(this, arguments);
+      const uniqTokens = _.uniq([].concat.apply([], om.updateBuffer));
+      om.notifyUpdate(uniqTokens);
+      om.updateBuffer = [];
       om.isUpdating = false;
       return result;
-  	}
-  	return fn.apply(this, arguments);
+    }
+    return fn.apply(this, arguments);
   };
   descriptor.value = newFn;
   return descriptor;
